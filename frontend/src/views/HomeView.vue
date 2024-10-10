2deaa5a66dad665f3caa39589580e35586ec34e9
Sebastian Wiemer <sebastian.wiemer@datev.de>
2024-10-10 11:22:35 +0200

    adds clean bootstrap5 example

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from 'vue'
+import api from '../api/backend';
+
+const message = ref('')
+
+function callApi() {
+  api.hello().then((response) => {
+    console.log("Response", response);
+    message.value = response.data;
+  })
+}
+
+onMounted(() => {
+  console.log("Calling api")
+  callApi();
+})
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>{{ message }}</h1>
   </main>
 </template>
